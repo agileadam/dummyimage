70b2e985aec1d6c2945822854581c00552d9d78a
Adam Courtemanche <adam.courtemanche@gmail.com>
2013-07-16 08:34:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+dummyimage
+==========
+
+Uses [http://dummyimage.com](http://dummyimage.com) to retrieve a specific number of random images within a certain range of dimensions, containing a specified label. This is useful for generating placeholder images for website development.
+
+## Tips
+* If you need images that are at an exact dimension, use the same min/max values. (e.g., `--min 100x100 --max 100x100`)
+* You can generate a single image using `-n 1`
+
+## Usage
+
+Please run `python dummyimage.py -h` to see the usage syntax and possible options.
+
+**All options**
+<pre>
+$ python dummyimage.py -i -t jpg -d ~/Desktop/result/ --min 100x200 --max 300x400 -n 3 -p Slide
+Retrieving 1 of 3 : http://dummyimage.com/281x385/77542A/6AB049.jpg&text=Slide+1+281×385
+Retrieving 2 of 3 : http://dummyimage.com/236x205/DE0C7B/7B6BC8.jpg&text=Slide+2+236×205
+Retrieving 3 of 3 : http://dummyimage.com/122x389/75FF05/934B3C.jpg&text=Slide+3+122×389
+</pre>
+
+**Only required options**
+<pre>
+$ python dummyimage.py -d ~/Desktop/result/ --min 100x200 --max 300x400 -n 3 -p Slide
+Retrieving 1 of 3 : http://dummyimage.com/132x363/FA7AC5/05A9E7.png&text=Slide+1
+Retrieving 2 of 3 : http://dummyimage.com/155x264/522D6A/F38EB8.png&text=Slide+2
+Retrieving 3 of 3 : http://dummyimage.com/121x272/F3F9B7/B7DC24.png&text=Slide+3
+</pre>
+
+## Todo
+1. Improve readability: foreground and background colors can be too similar on occasion.
+1. Error checking
---
+++ dummyimage.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import os
+import argparse
+import random
+import urllib2
+import sys
+
+parser = argparse.ArgumentParser(description="Uses http://dummyimage.com to \
+    retrieve a specific number of random images within a certain range of \
+    dimensions, containing a specified label. This is useful for generating \
+    placeholder images for website development.")
+
+parser.add_argument("-i", "--include-dimensions", dest="dimensionsInTitle",
+    default=False, action="store_true",
+    help="Optional. Include dimensions in title")
+
+parser.add_argument("-t", "--filetype", dest="filetype",
+    help="Optional. Output file type without leading period. Use png, jpg, or \
+    gif (png is default).", default="png", metavar="FILETYPE")
+
+parser.add_argument("-d", "--directory", dest="targetDirectory", required=True,
+    help="Output directory", metavar="DIRECTORY")
+
+parser.add_argument("--min", dest="minDimensions", required=True,
+    help="Minimum dimensions?", metavar="WxH")
+
+parser.add_argument("--max", dest="maxDimensions", required=True,
+    help="Maximum dimensions?", metavar="WxH")
+
+parser.add_argument("-n", "--num", type=int, dest="numImages", required=True,
+    help="How many images to generate.", metavar="INTEGER")
+
+parser.add_argument("-p", "--prefix", dest="prefix", required=True,
+    help="Prefix for the images' text overlay. Use Alphas, Numbers, Spaces, \
+    Underscores, and Hyphens only.", metavar="PREFIX")
+
+args = parser.parse_args()
+
+mins = args.minDimensions.split("x")
+maxes = args.maxDimensions.split("x")
+minWidth = int(mins[0])
+maxWidth = int(maxes[0])
+minHeight = int(mins[1])
+maxHeight = int(maxes[1])
+
+targetDirectory = os.path.expanduser(args.targetDirectory)
+if not os.access(targetDirectory, os.W_OK):
+    sys.exit("ERROR: Unable to write to output directory.")
+
+i = 0
+while i < args.numImages:
+    i += 1
+    r = lambda: random.randint(0,255)
+    bgcolor = "%02X%02X%02X" % (r(),r(),r())
+    fgcolor = "%02X%02X%02X" % (r(),r(),r())
+    width = random.randint(minWidth, maxWidth)
+    height = random.randint(minHeight, maxHeight)
+    dimensionsTitle = "" 
+    if args.dimensionsInTitle:
+        # The character here is not an X, it's a multiplication sign
+        dimensionsTitle = "+" + str(width) + "×" + str(height)
+    url = "http://dummyimage.com/%dx%d/%s/%s.%s&text=%s+%d%s" % (width,
+        height, bgcolor, fgcolor, args.filetype, args.prefix, i,
+        dimensionsTitle)
+    r = urllib2.urlopen(url)
+    print "Retrieving", i, "of", args.numImages, ":", url
+    output = open("%s/%s_%d.%s" % (targetDirectory,
+        args.prefix.lower().replace(" ","_"), i, args.filetype), "wb")
+    output.write(r.read())
+    output.close()
+
+sys.exit()
